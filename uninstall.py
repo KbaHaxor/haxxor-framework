cbe7409eedf324d5d803010ee0ef687d651c649e
R3C0Nx00 <taylor@puckettonline.net>
2016-06-02 06:54:40 -0400

    uninstall script

---
+++ uninstall.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+import sys
+import getpass
+import termcolor
+
+# VARIABLES #
+user = getpass.getuser()
+ipath = ("/usr/haxxor")
+haxxor = ("/usr/bin/haxxor")
+if(user == 'root'):
+	print colored("[*] Running as root", "yellow")
+else:
+	print colored("[!] Must run as root", "red")
+	sys.exit(0)
+if(os.path.exists(ipath)):
+	call("rm -r " + ipath, shell=True)
+else:
+	print colored("[!] Could not find %s", % ipath, "red")
+	sys.exit(0)
+if(os.path.isfile(haxxor)):
+	call("rm " + haxxor, shell=True)
+else:
+	print colored("[!] Coudl not find %s" % haxxor, "red")
+	sys.exit(0)
+
